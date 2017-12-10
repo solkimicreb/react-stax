98c5149f37f04b217b53153b8c49a740c482a4c5
Miklos Bertalan <miklos.bertalan@risingstack.com>
2017-12-11 00:41:03 +0100

    feat (params): improve type handling

--- src/params/searchParams.js
+++ src/params/searchParams.js
@@ -1,13 +1,10 @@
-import { toQueryTypes, toParamTypes } from './types'
-
 export function toQuery (params) {
-  const query = toQueryTypes(params)
   const queryTokens = []
 
-  for (let key in query) {
-    const value = query[key]
+  for (let key in params) {
+    const value = params[key]
     if (value !== undefined && value !== '') {
-      queryTokens.push(`${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
+      queryTokens.push(`${encodeURIComponent(key)}=${encodeURIComponent(JSON.stringify(value))}`)
     }
   }
   return queryTokens.length ? '?' + queryTokens.join('&') : ''
@@ -22,9 +19,9 @@ export function toParams (queryString) {
   const params = {}
   for (let token of queryTokens) {
     const keyValue = token.split('=')
-    params[decodeURIComponent(keyValue[0])] = decodeURIComponent(keyValue[1])
+    params[decodeURIComponent(keyValue[0])] = JSON.parse(decodeURIComponent(keyValue[1]))
   }
-  return toParamTypes(params)
+  return params
 }
 
 function notEmpty (string) {
