59c2448c14e3a2821de1d0959c18c690529a985a
Miklos Bertalan <miklos.bertalan@risingstack.com>
2018-05-19 01:21:52 +0200

    removed the UMD build

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # WIP: React Easy Stack
 
 I simple React stack with state management, routing, code splitting and animations.
+
+https://img.shields.io/bundlephobia/minzip/react-easy-stack.svg
--- scripts/build.js
+++ scripts/build.js
@@ -1,15 +1,15 @@
-const fs = require('fs')
-const path = require('path')
-const del = require('del')
-const babel = require('babel-core')
-const buble = require('buble')
-const rollup = require('rollup')
-const aliasPlugin = require('rollup-plugin-alias')
-const resolvePlugin = require('rollup-plugin-node-resolve')
-const babelPlugin = require('rollup-plugin-babel')
-const externalsPlugin = require('rollup-plugin-auto-external')
+const fs = require('fs');
+const path = require('path');
+const del = require('del');
+const babel = require('babel-core');
+const buble = require('buble');
+const rollup = require('rollup');
+const aliasPlugin = require('rollup-plugin-alias');
+const resolvePlugin = require('rollup-plugin-node-resolve');
+const babelPlugin = require('rollup-plugin-babel');
+const externalsPlugin = require('rollup-plugin-auto-external');
 
-const env = process.env.BUILD_ENV || 'browser'
+const env = process.env.BUILD_ENV || 'browser';
 
 const bundles = [
   {
@@ -18,9 +18,6 @@ const bundles = [
       plugins: [
         babelPlugin({ exclude: 'node_modules/**' }),
         resolvePlugin(),
-        aliasPlugin({
-          env: path.resolve('src/env', `${env}.js`)
-        }),
         externalsPlugin({ dependencies: true, peerDependecies: true })
       ]
     },
@@ -34,74 +31,54 @@ const bundles = [
       plugins: [
         babelPlugin({ exclude: 'node_modules/**' }),
         resolvePlugin(),
-        aliasPlugin({
-          env: path.resolve('src/env', `${env}.js`)
-        }),
         externalsPlugin({ dependencies: true, peerDependecies: true })
       ]
     },
     output: {
       format: 'cjs'
     }
-  },
-  {
-    input: {
-      input: path.resolve('src/index.js'),
-      plugins: [
-        babelPlugin({ exclude: 'node_modules/**' }),
-        resolvePlugin(),
-        aliasPlugin({
-          env: path.resolve('src/env', `${env}.js`)
-        }),
-        externalsPlugin({ dependencies: false, peerDependecies: true })
-      ]
-    },
-    output: {
-      format: 'umd',
-      name: 'easyStack'
-    }
   }
-]
+];
 
-async function build () {
+async function build() {
   // Clean up the output directory
-  await del(path.resolve('dist'))
-  fs.mkdirSync(path.resolve('dist'))
+  await del(path.resolve('dist'));
+  fs.mkdirSync(path.resolve('dist'));
 
   // Compile source code into a distributable format with Babel and Rollup
   for (const config of bundles) {
-    const es6Path = path.resolve('dist', `${config.output.format}.es6.js`)
-    const bundle = await rollup.rollup(config.input)
-    const { code: es6Code } = await bundle.generate(config.output)
-    fs.writeFileSync(es6Path, es6Code, 'utf-8')
+    const es6Path = path.resolve('dist', `${config.output.format}.es6.js`);
+    const bundle = await rollup.rollup(config.input);
+    const { code: es6Code } = await bundle.generate(config.output);
+    fs.writeFileSync(es6Path, es6Code, 'utf-8');
 
     const es6MinPath = path.resolve(
       'dist',
       `${config.output.format}.es6.min.js`
-    )
+    );
     const { code: es6MinCode } = babel.transform(es6Code, {
       presets: ['minify']
-    })
-    fs.writeFileSync(es6MinPath, es6MinCode, 'utf-8')
+    });
+    fs.writeFileSync(es6MinPath, es6MinCode, 'utf-8');
 
-    const es5Path = path.resolve('dist', `${config.output.format}.es5.js`)
+    const es5Path = path.resolve('dist', `${config.output.format}.es5.js`);
     const { code: es5Code } = buble.transform(es6Code, {
       transforms: {
         dangerousForOf: true,
         modules: false
       }
-    })
-    fs.writeFileSync(es5Path, es5Code, 'utf-8')
+    });
+    fs.writeFileSync(es5Path, es5Code, 'utf-8');
 
     const es5MinPath = path.resolve(
       'dist',
       `${config.output.format}.es5.min.js`
-    )
+    );
     const { code: es5MinCode } = babel.transform(es5Code, {
       presets: ['minify']
-    })
-    fs.writeFileSync(es5MinPath, es5MinCode, 'utf-8')
+    });
+    fs.writeFileSync(es5MinPath, es5MinCode, 'utf-8');
   }
 }
 
-build()
+build();
