f5dd50474d1977e4090eae7fd8132728447274ee
Miklos Bertalan <miklos.bertalan@risingstack.com>
2018-05-23 22:11:14 +0200

    docs: comment router utils

--- src/router/utils.js
+++ src/router/utils.js
@@ -1,29 +1,39 @@
+const notEmpty = token => token !== undefined && token !== '';
+
+// convert pathname strings to arrays
 export function toPathArray(path = '') {
   return path.split('/').filter(notEmpty);
 }
 
+// convert path arrays to absolute pathname strings
 export function toPathString(path = []) {
+  // the leading '/' is important, it differentiates absolute pathnames from relative ones
   return '/' + path.filter(notEmpty).join('/');
 }
 
+// convert params objects to query strings
 export function toQuery(params = {}) {
   const query = toObjectString(params);
-  return query ? `?${query}` : query;
+  return query ? `?${query}` : '';
 }
 
+// convert scroll options objects to URL hash
 export function toHash(scroll = {}) {
-  const { to, x, y } = scroll;
-  const hash = toObjectString({ to, x, y });
-  return hash ? `#${hash}` : hash;
+  // do not include none primitive options, like the scroll container
+  const { to, left, top } = scroll;
+  const hash = toObjectString({ to, left, top });
+  return hash ? `#${hash}` : '';
 }
 
+// converts objects to &key=value strings
 function toObjectString(params) {
   const tokens = [];
 
   for (let key in params) {
     let value = params[key];
-    if (value !== undefined && value !== '') {
+    if (notEmpty(value)) {
       key = encodeURIComponent(key);
+      // JSON stringify the value to keep the type information
       value = encodeURIComponent(JSON.stringify(value));
       tokens.push(`${key}=${value}`);
     }
@@ -31,8 +41,10 @@ function toObjectString(params) {
   return tokens.length ? tokens.join('&') : '';
 }
 
+// converts &key=value strings to objects
 export function toParams(queryString = '') {
   const queryTokens = queryString
+    // remove the leading ? or # character
     .slice(1)
     .split('&')
     .filter(notEmpty);
@@ -41,25 +53,25 @@ export function toParams(queryString = '') {
   for (let token of queryTokens) {
     const keyValue = token.split('=');
     const key = decodeURIComponent(keyValue[0]);
+    // JSON.stringify and parse keeps the type information
     const value = JSON.parse(decodeURIComponent(keyValue[1]));
     params[key] = value;
   }
   return params;
 }
 
-export function notEmpty(token) {
-  return token !== '';
-}
-
-export function addExtraProps(props, extraProps, defaultProps) {
+// augments the props with extraProps, which are not in excludeProps
+// nice for proxying irrelevant props to the underlying DOM element
+export function addExtraProps(props, extraProps, excludeProps) {
   for (let key in extraProps) {
-    if (!(key in defaultProps)) {
+    if (!(key in excludeProps)) {
       props[key] = extraProps[key];
     }
   }
   return props;
 }
 
+// detects if the current env is NodeJS
 export const isNode =
   typeof global === 'object' &&
   global.process &&
